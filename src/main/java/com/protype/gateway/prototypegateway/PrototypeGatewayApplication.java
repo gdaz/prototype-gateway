c928456dc8f15140740cb928ba9e7bc3fac96645
sathaphornTam <sathaphorn@loxbit.co.th>
2019-12-04 13:19:50 +0700

    init

---
+++ src/main/java/com/protype/gateway/prototypegateway/PrototypeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.protype.gateway.prototypegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeGatewayApplication.class, args);
+    }
+
+}
